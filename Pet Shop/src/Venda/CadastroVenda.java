2257f365b1e6e91455ef6c8423768618eb49c53c
gms4 <gms4@users.noreply.github.com>
2019-11-09 10:31:38 -0300

    repositorio lista finalizado

--- Pet Shop/src/Venda/CadastroVenda.java
+++ Pet Shop/src/Venda/CadastroVenda.java
@@ -5,22 +5,28 @@ public class CadastroVenda {
     	   repositorio = repo;
        }
        
-       public void cadastar (Vendas venda) throws LimiteAtingidoException {
-    	   if (!this.repositorio.existe(venda.getId())) {
-    		   this.repositorio.inserir(venda);
-    		   
+       public void cadastar (Vendas venda) 
+    		   throws LimiteAtingidoException {
+    	   //cadastrando qnd não existe a venda com o respectivo id
+    	   if (!this.repositorio.existe(venda.getId()))
+    		   this.repositorio.inserir(venda);	   
     	   }
-    		   
-    	   }
-       public void atualizar (Vendas venda) throws VendaNaoEncontradaException {
+       
+       public void atualizar (Vendas venda) 
+    		   throws VendaNaoEncontradaException {
     	   this.repositorio.atualizar(venda);
        }
-       public void remover (int id) throws VendaNaoEncontradaException {
+       
+       public void remover (int id) 
+    		   throws VendaNaoEncontradaException {
     	   this.repositorio.remover(id);
        }
-       public Vendas procurar (int id) throws VendaNaoEncontradaException {
+       
+       public Vendas procurar (int id) 
+    		   throws VendaNaoEncontradaException {
 		return this.repositorio.procurar(id);	   
        }
+       
        public boolean existe (int id) {
 		return this.repositorio.existe(id); 	   
        }
