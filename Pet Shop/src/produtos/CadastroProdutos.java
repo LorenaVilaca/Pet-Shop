3e3f80ae438e6c1570f710a88b6e429f1286f0c0
biel6 <biel6@172.22.42.238>
2019-11-11 16:57:04 -0300

    Repositorio em array atuualizado e classe Cadastro.

---
+++ Pet Shop/src/produtos/CadastroProdutos.java
@@ -0,0 +1,38 @@
+package produtos;
+
+public class CadastroProdutos {
+	private RepositorioProduto repositorio;
+	public CadastroProdutos(RepositorioProduto repositorio) {
+		this.repositorio = repositorio;
+	}
+
+	public void cadastrar(Produtos produto) { //produto ja cadastrado
+		if(!this.repositorio.existe(produto.getCode())) {
+			this.repositorio.inserir(produto);
+		}
+	}
+
+	public void atualizarPreco(String code, double price) {
+		this.repositorio.atualizarPreco(code, price);
+	}
+
+	public void atualizarQuantidade(String code, int quantidade) {
+		this.repositorio.atualizarQuantidade(code, quantidade);
+	}
+	public void atualizarFornecedor(String code, String fornecedor) {
+		this.repositorio.atualizarFornecedor(code, fornecedor);
+	}
+	public void remover(String codeProduto) {
+		this.repositorio.remover(codeProduto);
+	}
+
+	public Produtos procurar(String codeProduto) {
+		return this.repositorio.procurar(codeProduto);
+	}
+	
+	public boolean existe(String codeProduto) {
+		return this.repositorio.existe(codeProduto);
+	}
+	
+}
+
