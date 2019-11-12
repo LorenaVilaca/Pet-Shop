721507a6b799a60b9b08ec19964ecd2cce986d17
Thaís <tvc@cin.ufpe.br>
2019-11-12 15:29:28 -0200

    Implementando package clientes

--- Pet Shop/src/clientes/RepositorioClientesLista.java
+++ Pet Shop/src/clientes/RepositorioClientesLista.java
@@ -49,7 +49,7 @@ public class RepositorioClientesLista implements RepositorioClientes {
 		}
 
 	}
-
+//
 	public boolean existe(String cpf) {
 		if (this.cliente != null && this.cliente.getCpf().equals(cpf)) {
 			return true;
