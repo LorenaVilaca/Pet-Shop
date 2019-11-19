3bc4a68a1ecc7a5b90424369021d4b7050c84223
biel6 <biel6@192.168.15.11>
2019-11-19 18:08:26 -0300

    main beta 1.0

--- Pet Shop/src/programa/main.java
+++ Pet Shop/src/programa/main.java
@@ -1,10 +1,162 @@
 package programa;
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("Digite um numero correspondente a função que deseja alterar:\n "
+				+ "1 - FUNCIONARIOS\n"
+				+ " 2 - CLIENTES\n"
+				+ " 3 - PRODUTOS\n"
+				+ " 4 - SERVICOS\n"
+				+ " 5 - VENDAS");
+
+		//FUNCIONARIOS
+		if (in.nextInt() == 1) {
+			System.out.println("Selecione como deseja alterar FUNCIONARIOS:\n"
+					+ " 1 - Cadastrar\n"
+					+ " 2 - Procurar\n"
+					+ " 3 - Remover\n"
+					+ " 4 - Atualizar\n");
+
+			//CADASTRAR FUNCIONARIO
+			if (in.nextInt() == 1) {
+				
+			}
+
+			//PROCURAR FUNCIONARIO 
+			if (in.nextInt() == 2) {
+				
+			}
+
+			//REMOVER FUNCIONARIO
+			if (in.nextInt() == 3) {
+
+			}
+
+			//ATUALIZAR FUNIONARIO
+			if (in.nextInt() == 4) {
+
+			}
+		} 
+
+		//CLIENTES
+		if (in.nextInt() == 2) {
+			System.out.println("Selecione como deseja alterar CLIENTES:\n"
+					+ " 1 - Cadastrar\n"
+					+ " 2 - Procurar\n"
+					+ " 3 - Remover\n"
+					+ " 4 - Atualizar\n");
+			
+			//CADASTRAR CLIENTES
+			if (in.nextInt() == 1) {
+				
+			}
+
+			//PROCURAR CLIENTES 
+			if (in.nextInt() == 2) {
+				
+			}
+
+			//REMOVER CLIENTES
+			if (in.nextInt() == 3) {
+
+			}
+
+			//ATUALIZAR CLIENTES
+			if (in.nextInt() == 4) {
+
+			}
+		}
+
+		//PRODUTOS
+		if (in.nextInt() == 3) {
+			System.out.println("Selecione como deseja alterar PRODUTOS:\n"
+					+ " 1 - Cadastrar\n"
+					+ " 2 - Procurar\n"
+					+ " 3 - Remover\n"
+					+ " 4 - Atualizar\n");
+			
+			//CADASTRAR PRODUTOS
+			if (in.nextInt() == 1) {
+				
+			}
+
+			//PROCURAR PRODUTOS 
+			if (in.nextInt() == 2) {
+				
+			}
+
+			//REMOVER PRODUTOS
+			if (in.nextInt() == 3) {
+
+			}
+
+			//ATUALIZAR PRODUTOS
+			if (in.nextInt() == 4) {
+
+			}
+		}
+
+		//SERVICOS
+		if (in.nextInt() == 4) {
+			System.out.println("Selecione como deseja alterar SERVICOS:\n"
+					+ " 1 - Cadastrar\n"
+					+ " 2 - Procurar\n"
+					+ " 3 - Remover\n"
+					+ " 4 - Atualizar\n");
+			
+			//CADASTRAR FUNCIONARIO
+			if (in.nextInt() == 1) {
+				
+			}
+
+			//PROCURAR FUNCIONARIO 
+			if (in.nextInt() == 2) {
+				
+			}
+
+			//REMOVER FUNCIONARIO
+			if (in.nextInt() == 3) {
+
+			}
+
+			//ATUALIZAR FUNIONARIO
+			if (in.nextInt() == 4) {
+
+			}
+		}
+
+		//VENDAS
+		if (in.nextInt() == 5) {
+			System.out.println("Selecione como deseja alterar VENDAS:\n"
+					+ " 1 - Cadastrar\n"
+					+ " 2 - Procurar\n"
+					+ " 3 - Remover\n"
+					+ " 4 - Atualizar\n");
+			
+			//CADASTRAR FUNCIONARIO
+			if (in.nextInt() == 1) {
+				
+			}
+
+			//PROCURAR FUNCIONARIO 
+			if (in.nextInt() == 2) {
+				
+			}
+
+			//REMOVER FUNCIONARIO
+			if (in.nextInt() == 3) {
+
+			}
+
+			//ATUALIZAR FUNIONARIO
+			if (in.nextInt() == 4) {
+
+			}
+		}
 	}
 
 }
